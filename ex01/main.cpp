11c8cdf9671da168c05ea9ddbff0aaf5cfc2ef75
albalopeez21 <57994534+albalopeez21@users.noreply.github.com>
2025-05-22 18:45:00 +0200

    Add files via upload

---
+++ ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name);
+
+int main() {
+    int hordeSize = 5;
+    Zombie* horde = zombieHorde(hordeSize, "Horder");
+
+    if (!horde)
+        return 1;
+
+    for (int i = 0; i < hordeSize; ++i) {
+        horde[i].announce();
+    }
+
+    delete[] horde;
+    return 0;
+}
